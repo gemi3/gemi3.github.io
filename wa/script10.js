ec86459528edbf5fbeeba0ff9e1f3d7f92669a33
gemi3 <gemi3525@colorado.edu>
2024-03-21 23:45:00 -0600

    final

--- wa/script10.js
+++ wa/script10.js
@@ -11,11 +11,11 @@ function randomValueFromArray(array){
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-let insertx = ["Willy the Goblin","Big Daddy","Father Christmas"];
+let insertx = ["Bongo Bumblebee","Ziggy Zapman","Captain Cruncher"];
 
-let inserty = ["the soup kitchen", "Disneyland","the White House"];
+let inserty = ["Gizmo Grove", "Starlight Summit","Frostbite Falls"];
 
-let insertz = ["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"];
+let insertz = ["witnessed a parade of magical steam powered gizmos","began to make constellations out of the dust in the air","got lost in a maze of enchanted mirrors"];
 
 randomize.addEventListener('click', result);
 
